b85c5d822d679653fde053712b11ea275cc4f0fc
svoboda88 <zhenya.dowgun@gmail.com>
2022-04-30 03:53:01 +0500

    Feat: solve dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function createDreamTeam(members) {
+  if(!members?.length) return false;
+  
+  const arrOfStrings = members.filter((el) => {
+    return typeof el === 'string';
+  }).map(el => el.replaceAll(' ', ''));
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  const firstLettersArr = arrOfStrings.map((el) => el[0].toUpperCase()).sort();
+  if(!firstLettersArr.length) return false
+
+  return firstLettersArr.join('');
 }
 
 module.exports = {
