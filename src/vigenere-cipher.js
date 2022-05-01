80b74e21408ac3fe171530b557d33b9ff0807027
svoboda88 <zhenya.dowgun@gmail.com>
2022-05-01 17:46:03 +0500

    feat: done vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(name=true) {
+      this.name = name;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+      if (message === undefined || key === undefined ) throw new Error(`Incorrect arguments!`);
+
+      while (key.length < message.length) {
+          key += key;
+      }
+
+      key=key.toUpperCase().split('');
+      message = message.toUpperCase().split('');
+      
+      message = message.map((el, index) => {
+          if (el.charCodeAt()<65 || el.charCodeAt()>90) {
+              key.splice(index, 0, ' ');
+              return el;
+          }
+          let code = el.charCodeAt() + key[index].charCodeAt() - 65;
+          if (code <=90) {
+              return String.fromCharCode(code)
+          } else return String.fromCharCode(code-26);
+      })
+
+     return this.name ? message.join('') : message.reverse().join('');
   }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined ) throw new Error(`Incorrect arguments!`);
+      while (key.length < message.length) {
+          key += key;
+      }
+
+      key=key.toUpperCase().split('');
+      message = message.toUpperCase().split('');
+      
+      message = message.map((el, index) => {
+          if (el.charCodeAt()<65 || el.charCodeAt()>90) {
+              key.splice(index, 0, ' ');
+              return el;
+          }
+          let code = el.charCodeAt() - key[index].charCodeAt() + 65;
+          if (code >=65) {
+              return String.fromCharCode(code)
+          } else return String.fromCharCode(code+26);
+      })
+
+    return this.name ? message.join('') : message.reverse().join('');
+      }
+      
 }
 
 module.exports = {
