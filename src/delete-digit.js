2d8b1160c40e53fcf5b934507598439422edb967
svoboda88 <zhenya.dowgun@gmail.com>
2022-05-02 00:16:59 +0500

    fix: better solution for delete-digit task

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,17 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
- function deleteDigit(n) {
-  let arrOfNumbers = Array.from(n.toString(), String);
-  const l= arrOfNumbers.length;
-  let newArr = [];
-  for (let i=0; i<l; i++) {
-      arrOfNumbers = Array.from(n.toString(), String);
-      arrOfNumbers.splice(i, 1);    
-      newArr.push(arrOfNumbers.join(''));
-  }
-  console.log(newArr);
-  return Math.max(...newArr);
+//  function deleteDigit(n) {
+//   let arrOfNumbers = Array.from(n.toString(), String);
+//   const l= arrOfNumbers.length;
+//   let newArr = [];
+//   for (let i=0; i<l; i++) {
+//       arrOfNumbers = Array.from(n.toString(), String);
+//       arrOfNumbers.splice(i, 1);    
+//       newArr.push(arrOfNumbers.join(''));
+//   }
+//   console.log(newArr);
+//   return Math.max(...newArr);
+// }
+
+function deleteDigit(n) {
+  const numberStr = n + '';
+  const arr = numberStr.split('');
+
+  const kekArr = arr.map((el, index) => {
+    return Number(numberStr.slice(0, index) + numberStr.slice(index + 1, numberStr.length));
+  });
+
+  return Math.max(...kekArr)
 }
 
 module.exports = {
